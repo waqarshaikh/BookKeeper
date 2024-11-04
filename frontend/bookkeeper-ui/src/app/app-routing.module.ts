ba35a678c58b28acd18054a28ad16546fccefa75
waqarshaikh <shaikhwaqar1999@gmail.com>
2024-11-04 16:08:24 -0500

    ENH: Add angular routing

--- frontend/bookkeeper-ui/src/app/app-routing.module.ts
+++ frontend/bookkeeper-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BookListComponent} from "./book-list/book-list.component";
+import {AddBookFormComponent} from "./add-book-form/add-book-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/add', component: AddBookFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
