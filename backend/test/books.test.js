0053f455df82d97d6767a0aaf01d02d4bdf37531
waqarshaikh <shaikhwaqar1999@gmail.com>
2024-11-04 14:12:27 -0500

    TST: Add data cleanup for proper testing

--- backend/test/books.test.js
+++ backend/test/books.test.js
@@ -1,7 +1,14 @@
 const request = require('supertest');
 const app = require('../src/index');
+const db = require("../db/db");
 
 describe('GET api/books', () => {
+    beforeAll(async () => {
+        await db.query('DELETE FROM Inventory');
+        await db.query('INSERT INTO Inventory (title, author, publication_date, genre, isbn) VALUES (?, ?, ?, ?, ?)',
+            ['Test Book', 'Test Author', '2024-01-01', 'Test Genre', '1234567890']);
+    });
+
     it('should return a list of books', async () => {
         const response = await request(app)
             .get('/api/books')
@@ -15,7 +22,7 @@ describe('GET api/books', () => {
         const response = await request(app)
             .get('/api/books')
             .set('Accept', 'application/json');
-
+        console.log(response.body);
         const book = response.body[0];
         expect(book).toHaveProperty('entry_id');
         expect(book).toHaveProperty('title');
@@ -27,6 +34,15 @@ describe('GET api/books', () => {
 });
 
 describe('POST api/books', () => {
+    beforeAll(async () => {
+        await db.query('DELETE FROM Inventory');
+    });
+
+    afterAll(async () => {
+        await db.query('DELETE FROM Inventory');
+    });
+
+
     it('should add a new book and return it', async () => {
         const newBook = {
             title: 'New Book Title',
